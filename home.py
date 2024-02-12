4df85d8b9d3aba00081611e88679b4b342b83c7c
sandeepbhuvan <sandeepbhuvan@gmail.com>
2024-02-12 18:03:16 +0530

    something new

--- home.py
+++ home.py
@@ -1,9 +1,13 @@
-TEMPLATE="""
-Hello {}"""
-
+from jinja2 import Template
 
 def main():
-    print(TEMPLATE.format("Sandeep"))
+    file = open("login.j2")
+    template = Template(file.read())
+    file.close()
+    rendered_template = template.render()
+    output_file = open("output.html", "w")
+    output_file.write(rendered_template)
+    output_file.close()
 
 if __name__ == "__main__":
     main()
